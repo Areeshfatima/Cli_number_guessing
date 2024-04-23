99f82815dfe17005c7b208986185687a4ab04bf9
Areeshfatima <muslimahvibes.696@gmail.com>
2024-04-23 08:05:25 +0500

    cli number guessing game publish on github

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const random_number = Math.floor(Math.random() * 6 + 1);
+console.log("Welcome To Number Guessing Game...");
+const answer = await inquirer.prompt([{
+        name: "userNumberGuessed",
+        type: "number",
+        message: "Kindly guess any number between 1-6",
+    }]);
+if (answer.userNumberGuessed === random_number) {
+    console.log("Congratulation You guessed a correct number..");
+}
+else {
+    console.log("Opps hard luck you guessed a wrong number..");
+}
